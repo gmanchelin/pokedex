b50a5c518e4c5b39952a1e7b02664c07503a0380
Gabriel Manchelin <gmanchelin@gmail.com>
2024-07-29 00:07:23 +0200

    Background color for types moved in types-color.ts

--- src/components/TypeIcon.tsx
+++ src/components/TypeIcon.tsx
@@ -1,31 +1,11 @@
 import { Grid, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
+import { TypesColor } from "../types-color";
 
 interface TypeIconProps {
   type: string;
 }
 function TypeIcon(props: TypeIconProps) {
-  const backgroundColor: { [key: string]: string } = {
-    bug: "#91a119",
-    dark: "#50413f",
-    dragon: "#5061e1",
-    electric: "#fac000",
-    fairy: "#f170f1",
-    fighting: "#ff8000",
-    fire: "#e62829",
-    flying: "#81b9ef",
-    ghost: "#704170",
-    grass: "#42a129",
-    ground: "#915121",
-    ice: "#3fd8ff",
-    normal: "#9fa19f",
-    poison: "#9040cc",
-    psychic: "#ef4179",
-    rock: "#afa981",
-    steel: "#60a1b8",
-    water: "#2980ef",
-  };
-  console.log(props.type);
   const typeSvg = `src/assets/types_icons/${props.type}.svg`;
   return (
     <Grid
@@ -33,7 +13,7 @@ function TypeIcon(props: TypeIconProps) {
       display={"flex"}
       sx={{
         borderRadius: "15px",
-        backgroundColor: backgroundColor[props.type],
+        backgroundColor: TypesColor[props.type],
         height: "30px",
         width: "150px",
         alignItems: "center",
