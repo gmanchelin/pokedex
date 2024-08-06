af69beb6b7595753d9f0331b097d530e7f1402a1
gmanchelin <gmanchelin@gmail.com>
2024-08-06 08:28:19 +0200

    React router dom on project

---
+++ src/models/PokemonDetails.tsx
@@ -0,0 +1,26 @@
+export interface PokemonDetails {
+  id: number;
+  species: {
+    name: string;
+  };
+  sprites: {
+    other: {
+      home: {
+        front_default: string;
+        front_shiny: string;
+      };
+    };
+  };
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+  abilities: {
+    ability: {
+      name: string;
+    };
+    is_hidden: boolean;
+    slot: number;
+  }[];
+}
