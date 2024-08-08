d783a3a3eea3944756343092742d7b41c18bec81
gmanchelin <gmanchelin@gmail.com>
2024-08-08 07:46:13 +0200

    Function moved

--- src/models/SharedValues.tsx
+++ src/models/SharedValues.tsx
@@ -1,11 +1,4 @@
 export const FIRST_POKEMON_ID: number = 1;
 export const LAST_POKEMON_ID: number = 1025;
 
-export function CapitalizeAndRemoveHyphen(str: string) {
-  const words = str.split("-");
-  return words
-    .map((word) => {
-      return word[0].toUpperCase() + word.substring(1);
-    })
-    .join(" ");
-}
+
