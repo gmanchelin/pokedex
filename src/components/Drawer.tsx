f4c45e0639e067c9800b84c84f620a5d6f57bd86
gmanchelin <gmanchelin@gmail.com>
2024-08-08 07:48:07 +0200

    Changing icon

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -7,9 +7,8 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
+import QuizIcon from "@mui/icons-material/Quiz";
 import { useNavigate } from "react-router-dom";
 
 export default function TemporaryDrawer() {
@@ -34,7 +33,7 @@ export default function TemporaryDrawer() {
             <ListItem key={text} disablePadding>
               <ListItemButton onClick={() => navigate(path)}>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  {index % 2 === 0 ? <QuizIcon /> : <QuizIcon />}
                 </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
