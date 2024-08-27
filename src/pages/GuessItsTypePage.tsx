e9921d9b71e0a394f74978e65855f4da40b234b0
Gabriel Manchelin <75973031+gmanchelin@users.noreply.github.com>
2024-08-27 17:27:59 +0200

    localStorage for scores, tooltip with rules, streak score conditionning elements

--- src/pages/GuessItsTypePage.tsx
+++ src/pages/GuessItsTypePage.tsx
@@ -3,13 +3,16 @@ import { Pokemon } from "../models/Pokemon";
 import { LAST_POKEMON_ID } from "../models/SharedValues";
 import { getPokemon } from "../models/SharedFunctions";
 import TypeIcon from "../components/TypeIcon";
+import InfoIcon from '@mui/icons-material/Info';
 import {
   Alert,
   Box,
   Button,
   Grid,
+  IconButton,
   Snackbar,
   SnackbarCloseReason,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import Sprite from "../components/Sprite";
@@ -18,12 +21,13 @@ function GuessItsTypePage() {
   const [id, setId] = useState<number>(randomNumber);
   const [pokemon, setPokemon] = useState<Pokemon>();
   const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
-  const [numberOfTries, setNumberOfTries] = useState<number>(0);
-  const [numberOfGoodAnswers, setNumberOfGoodAnswers] = useState<number>(0);
+  const [numberOfTries, setNumberOfTries] = useState<number>(localStorage.getItem("numberOfTries") ? parseInt(localStorage.getItem("numberOfTries")!) : 0);
+  const [streak, setStreak] = useState<number>(localStorage.getItem("streak") ? parseInt(localStorage.getItem("streak")!) : 0);
+  const [numberOfGoodAnswers, setNumberOfGoodAnswers] = useState<number>(localStorage.getItem("numberOfGoodAnswers") ? parseInt(localStorage.getItem("numberOfGoodAnswers")!) : 0);
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [displayedMessage, setDisplayedMessage] = useState<string>("");
   const [isGoodAnswer, setIsGoodAnswer] = useState<boolean>(false);
-  
+
   const types = [
     "normal",
     "grass",
@@ -46,11 +50,15 @@ function GuessItsTypePage() {
   ];
 
   useEffect(() => {
-    async function fetchData() {
-      setPokemon(await getPokemon(id));
-    }
+    localStorage.setItem("numberOfTries", numberOfTries.toString());
+    localStorage.setItem("numberOfGoodAnswers", numberOfGoodAnswers.toString());
+    localStorage.setItem("streak", streak.toString());
     fetchData();
-  }, [id]);
+  }, [id, numberOfTries, numberOfGoodAnswers]);
+
+  async function fetchData() {
+    setPokemon(await getPokemon(id));
+  }
 
   function randomNumber() {
     return 1 + Math.floor(Math.random() * LAST_POKEMON_ID);
@@ -65,13 +73,14 @@ function GuessItsTypePage() {
       setIsGoodAnswer(true);
       setDisplayedMessage("DING DING! Good answer!");
       setNumberOfGoodAnswers(numberOfGoodAnswers + 1);
+      setStreak(streak + 1);
     } else {
       setIsGoodAnswer(false);
       setDisplayedMessage(
-        `BZZZT! Wrong answer! ${pokemon?.species.name.toUpperCase()} ${
-          pokemonTypes.length == 1 ? "type is" : "types are"
+        `BZZZT! Wrong answer! ${pokemon?.species.name.toUpperCase()} ${pokemonTypes.length == 1 ? "type is" : "types are"
         } ${pokemonTypes.join(", ").toUpperCase()}`
       );
+      setStreak(0);
     }
     setOpenSnackbar(true);
     setSelectedTypes([]);
@@ -94,7 +103,7 @@ function GuessItsTypePage() {
     if (selectedTypes.includes(type)) {
       setSelectedTypes(selectedTypes.filter((element) => element !== type));
     } else {
-      setSelectedTypes([...selectedTypes, type]);
+      setSelectedTypes(selectedTypes.length === 2 ? [selectedTypes[0], type] : [...selectedTypes, type]);
     }
     //Si déjà présent dans selectedType on retire, sinon on ajoute
   };
@@ -110,23 +119,28 @@ function GuessItsTypePage() {
         <Grid item>
           <Typography variant="h4" textAlign={"center"}>
             Guess Its Type
+            <Tooltip
+              title={
+                <>
+                  A random Pokémon appears! You have to guess its type(s) by selecting up to 2 of them. <br />
+                  You're helped with the number of types of the Pokémon at first, but things get more spicy as you progress with your streak of good guesses:<br />
+                  - Streak of 10 : the number of types is no more displayed<br />
+                  - Streak of 30 : the Pokémon's sprite is changed with a gray silhouette <br />
+                  - Streak of 50 : only the name of the Pokemon is displayed
+                </>
+              }
+            >
+              <IconButton sx={{ "&:hover": { cursor: "pointer" } }}>
+                <InfoIcon />
+              </IconButton>
+            </Tooltip>
           </Typography>
         </Grid>
         <Grid item>
-          <Box
-            component={"p"}
-            textAlign={"center"}
-            justifyContent={"space-around"}
-            display={"flex"}
-          >
-            <Typography>Number of tries : {numberOfTries}</Typography>
-            <Typography>Good answers : {numberOfGoodAnswers}</Typography>
-          </Box>
-        </Grid>
-
-        {pokemon?.sprites && (
-          <Grid item>
-            <Sprite pokemon={pokemon} height={200} width={200} />
+          {pokemon && streak < 50 && (
+            <Sprite pokemon={pokemon} height={200} width={200} isGray={streak > 30} />
+          )}
+          {pokemon && (
             <Typography
               variant="h4"
               textTransform={"capitalize"}
@@ -134,9 +148,12 @@ function GuessItsTypePage() {
             >
               {pokemon!.species.name}
             </Typography>
-          </Grid>
-        )}
-      </Grid>
+          )}
+          {pokemon && streak < 10 &&
+            <Typography textAlign="center"> Number of types : {pokemon!.types.length}</Typography>
+          }
+        </Grid>
+      </Grid >
       <Box component={"p"} textAlign={"center"}>
         <Grid container spacing={2} justifyContent={"center"}>
           {types.map((type) => (
@@ -156,21 +173,33 @@ function GuessItsTypePage() {
             >
               Guess
             </Button>
-            <Snackbar
-              open={openSnackbar}
-              autoHideDuration={6000}
-              onClose={handleClose}
-            >
-              <Alert
-                onClose={handleClose}
-                severity={isGoodAnswer ? "success" : "error"}
-                variant="filled"
-                sx={{ width: "100%" }}
-              >
-                {displayedMessage}
-              </Alert>
-            </Snackbar>
           </Grid>
+          <Grid item>
+            <Box
+              component={"p"}
+              textAlign={"center"}
+              justifyContent={"space-around"}
+              display={"flex"}
+            >
+              <Typography>Number of tries : {numberOfTries}</Typography>
+              <Typography>Good answers : {numberOfGoodAnswers}</Typography>
+              <Typography>Streak : {streak}</Typography>
+            </Box>
+          </Grid>
+          <Snackbar
+            open={openSnackbar}
+            autoHideDuration={6000}
+            onClose={handleClose}
+          >
+            <Alert
+              onClose={handleClose}
+              severity={isGoodAnswer ? "success" : "error"}
+              variant="filled"
+              sx={{ width: "100%" }}
+            >
+              {displayedMessage}
+            </Alert>
+          </Snackbar>
         </Grid>
       </Box>
     </>
