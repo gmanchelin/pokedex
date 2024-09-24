6ce88918c539e954fee81504cd440505936ae225
Gabriel Manchelin <75973031+gmanchelin@users.noreply.github.com>
2024-09-24 17:27:39 +0200

    Theme update for textfields and links

--- src/theme.tsx
+++ src/theme.tsx
@@ -5,7 +5,7 @@ declare module "@mui/material/styles" {
     conditional: {
       true: string;
       false: string;
-    };
+    }
   }
 }
 
@@ -14,25 +14,26 @@ export const getDesignTokens = (mode: PaletteMode) => ({
     mode,
     ...(mode === "light"
       ? {
-          background: {
-            paper: "#e4e4e4",
-            default: "#ffffff",
-          },
-          text: {
-            primary: "#373737",
-          },
-        }
+        background: {
+          paper: "#e4e4e4",
+          default: "#ffffff",
+        },
+        text: {
+          primary: "#373737",
+        },
+      }
       : {
-          background: {
-            paper: "#373737",
-            default: "#242424",
-          },
-          text: {
-            primary: "#e4e4e4",
-          },
-        }),
+        background: {
+          paper: "#373737",
+          default: "#242424",
+        },
+        text: {
+          primary: "#e4e4e4",
+        },
+      }),
     secondary: {
       main: "#c71717",
+      dark: '#8A1010'
     },
     text: {
       secondary: "#ffffff",
@@ -40,7 +41,7 @@ export const getDesignTokens = (mode: PaletteMode) => ({
     conditional: {
       true: "#ffffff",
       false: "#242424",
-    },
+    }
   },
   typography: {
     h1: {
@@ -62,13 +63,17 @@ export const getDesignTokens = (mode: PaletteMode) => ({
         root: ({ theme }: { theme: Theme }) => ({
           backgroundColor: theme.palette.secondary.main,
           color: theme.palette.text.secondary,
+          borderRadius: 40,
           '&.Mui-disabled': {
             backgroundColor: theme.palette.action.disabledBackground,
             color: theme.palette.text.disabled,
           },
           '&.no-theme-style': {
-          backgroundColor: 'inherit',
-        },
+            backgroundColor: 'inherit',
+          },
+          '&:hover': {
+            backgroundColor: theme.palette.secondary.dark,
+          },
         }),
       },
     },
@@ -82,6 +87,51 @@ export const getDesignTokens = (mode: PaletteMode) => ({
               color: theme.palette.secondary.main
             },
           };
+        },
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: (props: { theme: any; }) => {
+          const { theme } = props;
+          return {
+            color: theme.palette.secondary.main,
+            fontWeight: 'bold',
+            textDecoration: 'none',
+          };
+        },
+        underlineAlways: (props: { theme: any; }) => {
+          const { theme } = props;
+          return {
+            color: theme.palette.secondary.main,
+          }
+        }
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: (props: { theme: any; }) => {
+          const { theme } = props;
+          return {
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: theme.palette.secondary.dark, // Couleur de la bordure par défaut
+                borderRadius: 40
+              },
+              '&:hover fieldset': {
+                borderColor: theme.palette.secondary.dark, // Couleur de la bordure au hover
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: theme.palette.secondary.main, // Couleur de la bordure quand le champ est focus
+              },
+            },
+            '& .MuiInputLabel-root': {
+              color: theme.palette.secondary.main, // Couleur du label
+            },
+            '& .MuiInputBase-input': {
+              color: theme.palette.text.primary, // Couleur du texte à l'intérieur
+            },
+          }
         },
       },
     }
